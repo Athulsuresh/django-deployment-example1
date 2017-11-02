89b5e4c1ce6042793d85ae113b5bce46659166a5
athul suresh <athul.suresh@ymail.com>
2017-11-02 21:40:08 +0530

    first commit

---
+++ learning_templates/basic_app/templatetag/my_extras.py
@@ -0,0 +1,14 @@
+from django import template
+
+
+register = template.Library()
+
+@register.filter(name='cut')
+def cut(value,arg):
+    """
+   this  cuts all the values of 'arg' from the string
+    """
+
+    return value.replace(arg,'')
+
+#register.filter('cut',cut)
